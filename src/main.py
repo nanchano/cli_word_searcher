11a17d1641f00f8099b02d1dc908e8c1a88526e6
nanchano <nicolas.anchanio@gmail.com>
2021-05-01 19:04:03 -0300

    folder restructure

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from parser.parser import Parser
+from filesystem_dicts.file_dicts import *
+
+def main() -> None:
+    file_dict = FileDict('inputs/')
+    dir_dict = DirDict('inputs/')
+    
+    return
+
+if __name__ == '__main__':
+    main()
