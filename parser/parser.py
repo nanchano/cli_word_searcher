c9e1a1da73f769224c5b68c82f19552e919838ba
nanchano <nicolas.anchanio@gmail.com>
2021-05-01 19:01:38 -0300

    parser init update

--- parser/parser.py
+++ parser/parser.py
@@ -5,16 +5,16 @@ import random # just to print samples of the set
 
 class Parser(object):
 
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-        self.dir_name, self.file_name = ntpath.split(file_path)
+    def __init__(self, dir_name: str) -> None:
+        self.dir_name = dir_name
 
-    
-    def parse(self) -> list:
-        if not self._file_exists(self.file_path):
-            raise FileNotFoundError('No file named {} in directory {}'.format(self.file_name, self.dir_name))
+    def parse(self, file_name: str) -> set:
+        full_path = self.dir_name + file_name
 
-        with open(self.file_path) as f:
+        if not self._file_exists(full_path):
+            raise FileNotFoundError('No file named {} in directory {}'.format(file_name, self.dir_name))
+
+        with open(full_path) as f:
             # keeping only alphanumerical characters here
             # set since we only need one instance of each word (or number) to check if input is present
             self.data = {re.sub(r'[\W_]+', '', word.lower()) for line in f for word in line.split()}
@@ -38,7 +38,7 @@ class Parser(object):
         return os.path.isdir(dir_)
 
 if __name__ == '__main__':
-    #parser = Parser('../inputs/hamlet_TXT_FolgerShakespeare.txt')
-    parser = Parser('wrong_input.txt')
-    data = parser.parse()
+    parser = Parser('../inputs/')
+    #data = parser.parse('hamlet_TXT_FolgerShakespeare.txt')
+    data = parser.parse('doesntexist.txt')
     print(random.sample(data, 10))
