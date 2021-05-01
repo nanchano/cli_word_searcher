0cbc8be71b413f329bdb80f8ae55bd6777589b27
nanchano <nicolas.anchanio@gmail.com>
2021-05-01 19:37:38 -0300

    parser from dir name to full path

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -5,16 +5,15 @@ import random # just to print samples of the set
 
 class Parser(object):
 
-    def __init__(self, dir_name: str) -> None:
-        self.dir_name = dir_name
+    def __init__(self, full_path: str) -> None:
+        self.full_path = full_path
+        self.dir_name, self.file_name = ntpath.split(self.full_path)
 
-    def parse(self, file_name: str) -> set:
-        full_path = self.dir_name + file_name
+    def parse(self) -> set:
+        if not self._file_exists(self.full_path):
+            raise FileNotFoundError('No file named {} in {} directory'.format(self.file_name, self.dir_name))
 
-        if not self._file_exists(full_path):
-            raise FileNotFoundError('No file named {} in directory {}'.format(file_name, self.dir_name))
-
-        with open(full_path) as f:
+        with open(self.full_path) as f:
             # keeping only alphanumerical characters here
             # set since we only need one instance of each word (or number) to check if input is present
             self.data = {re.sub(r'[\W_]+', '', word.lower()) for line in f for word in line.split()}
@@ -24,7 +23,8 @@ class Parser(object):
     def write(self, output_path: str) -> None:
         output_dir, output_file_name = ntpath.split(output_path)
         if not self._dir_exists(output_dir):
-            raise FileNotFoundError('Directory {} does not exist'.format(output_dir))
+            print('Directory does not exist. Creating it...')
+            os.mkdir(output_dir)
 
         with open(output_path, 'w') as f:
             f.write(self.data)
@@ -38,7 +38,7 @@ class Parser(object):
         return os.path.isdir(dir_)
 
 if __name__ == '__main__':
-    parser = Parser('../inputs/')
-    #data = parser.parse('hamlet_TXT_FolgerShakespeare.txt')
-    data = parser.parse('doesntexist.txt')
+    #parser = Parser('../../inputs/doesntexist.txt')
+    parser = Parser('../inputs/hamlet_TXT_FolgerShakespeare.txt')
+    data = parser.parse()
     print(random.sample(data, 10))
