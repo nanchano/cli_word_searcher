1c705f210c90e99065d8d0914c177411cdc6a416
nanchano <nicolas.anchanio@gmail.com>
2021-05-02 18:25:45 -0300

    app docstrings

--- src/app/app.py
+++ src/app/app.py
@@ -6,7 +6,7 @@ from filesystem_dicts.file_dicts import *
 from prompt_toolkit.validation import Validator, ValidationError
 
 class App(object):
-
+    """CLI Application."""
     START_QUESTION = [
         {
             'type': 'list',
@@ -32,12 +32,14 @@ class App(object):
     ]
 
     def _print_help(self):
+        """Prints help message."""
         msg = '''A CLI tool that searches words inside the files on a given directory and ranks them based on their presence ratio, returning a list of the top 10 files that contain the most words.'''
         print(msg)
 
         return
 
     def _eval_start_options(self):
+        """Evauates starting options based on user input."""
         ans = prompt(self.START_QUESTION, style=custom_style_2).get('start_options')
 
         if ans == 'Exit':
@@ -49,6 +51,7 @@ class App(object):
 
 
     def get_answers(self):
+        """Get answers based on user responses to the prompts."""
         self.answers = prompt(self.QUESTIONS, style=custom_style_2)
 
         self.chosen_dir = self.answers.get('directory_choice')
@@ -57,12 +60,24 @@ class App(object):
         return self.chosen_dir, self.chosen_words.split() # default is str, return list of words
 
     def get_dir_data(self, dir_: str) -> dict:
+        """Gets each file data on the directory
+        Args:
+            dir_ (str): directory to search on.
+        Returns:
+            dict
+        """
         self.dir_dict = DirDict(dir_)
         self.data = self.dir_dict.load_files()
 
         return self.data
 
     def count_words(self, words: list, data: dict) -> None:
+        """
+        Count each word on each file.
+        Args:
+            words (list): elements to search.
+            data (dict): data to search on.
+        """
         self.counter = WordCounter(words)
         self.counter.count(data)
         self.counter.print_top(10)
@@ -70,6 +85,7 @@ class App(object):
         return
 
     def run(self):
+        """Runs the app."""
         print('welcome to the CLI word searcher!')
 
         self._eval_start_options()
