143c77fe0cf3bb7fec9d552935bc701b8b041b91
nanchano <nicolas.anchanio@gmail.com>
2021-05-02 15:37:00 -0300

    main updates

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-from parser.parser import Parser
+from parsing.parser import Parser
+from counting.counter import WordCounter
 from filesystem_dicts.file_dicts import *
 
+
 def main() -> None:
-    file_dict = FileDict('inputs/')
-    dir_dict = DirDict('inputs/')
+    dir_dict = DirDict('../inputs/')
+    #print(dir_dict.files)
+    data = dir_dict.load_files()
+    #print(data.keys())
+    #print(data.values())
     
+    counter = WordCounter(['shadows', 'offended', 'mended', 'command'])
+    counter.count(data)
+    counter.print_top(10)
+
+
     return
 
 if __name__ == '__main__':
