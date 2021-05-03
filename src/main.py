2d44b72986b603a563ea2e6c4e5ffb4b4164edcf
nanchano <nicolas.anchanio@gmail.com>
2021-05-02 21:21:19 -0300

    app run from main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,8 @@
-from parsing.parser import Parser
-from counting.counter import WordCounter
-from filesystem_dicts.file_dicts import *
-
+from app.app import App
 
 def main() -> None:
-    dir_dict = DirDict('../inputs/')
-    #print(dir_dict.files)
-    data = dir_dict.load_files()
-    #print(data.keys())
-    #print(data.values())
-    
-    counter = WordCounter(['shadows', 'offended', 'mended', 'command'])
-    counter.count(data)
-    counter.print_top(10)
-
+    app = App()
+    app.run()
 
     return
 
