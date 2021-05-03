b11958b63adcbdde8d5157139c2de909387d4fee
nanchano <nicolas.anchanio@gmail.com>
2021-05-02 22:45:01 -0300

    app update - recursion

--- src/app/app.py
+++ src/app/app.py
@@ -30,6 +30,15 @@ class App(object):
 
     ]
 
+    LAST_QUESTION = [
+        {
+            'type': 'list',
+            'name': 'final_options',
+            'message': 'Select an option.',
+            'choices': ['Search again', 'Change directory', 'Exit']
+        }
+    ]
+
     def _print_help(self):
         """Prints help message."""
         msg = '''A CLI tool that searches words inside the files on a given directory and ranks them based on their presence ratio, returning a list of the top 10 files that contain the most words.'''
@@ -48,14 +57,32 @@ class App(object):
         else:
             return
 
+    def _eval_final_options(self):
+        """Evaluates final options based on user input. Recursively runs the app again until exit choice."""
+        ans = prompt(self.LAST_QUESTION, style=custom_style_2).get('final_options')
+ 
+        if ans == 'Search again': 
+            self.chosen_words = prompt(self.QUESTIONS[1], style=custom_style_2).get('words_choice').split()
+            self.count_words(self.chosen_words, self.data)
+            self._eval_final_options()
 
-    def get_answers(self):
+        elif ans == 'Change directory':
+            self.chosen_dir, self.chosen_words = self.get_answers()
+            self.data = self.get_dir_data(self.chosen_dir)
+            self.count_words(self.chosen_words, self.data)
+            self._eval_final_options()
+
+        else:
+            exit()
+
+        return ans
+
+    def get_answers(self) -> tuple:
         """Get answers based on user responses to the prompts."""
         self.answers = prompt(self.QUESTIONS, style=custom_style_2)
 
         self.chosen_dir = self.answers.get('directory_choice')
         self.chosen_words = self.answers.get('words_choice')
-
         return self.chosen_dir, self.chosen_words.split() # default is str, return list of words
 
     def get_dir_data(self, dir_: str) -> dict:
@@ -90,8 +117,9 @@ class App(object):
         self._eval_start_options()
 
         self.chosen_dir, self.chosen_words = self.get_answers()
-        
         self.data = self.get_dir_data(self.chosen_dir)
         self.count_words(self.chosen_words, self.data)
+
+        self._eval_final_options()
 
         return
